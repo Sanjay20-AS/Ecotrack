ccf0dce37e82678d25b2101c819b3f2746674247
Sanjay A <sanjay20.as@gmail.com>
2026-04-03 08:13:26 +0530

    Deleted maven-wrapper.properties

--- backend/fix_null_roles.sql
+++
@@ -1,5 +0,0 @@
--- Update any existing users with null roles to have DONOR role
-UPDATE users SET role = 'DONOR' WHERE role IS NULL;
-
--- Check the results
-SELECT id, name, email, role FROM users;
